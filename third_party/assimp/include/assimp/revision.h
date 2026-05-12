6b5e3d8004569a98db63081fe15af5b1acd1c5d8
Kendal Harland <kendalharland@gmail>
2026-05-12 16:53:20 -0700

    Update third_party

--- third_party/assimp/include/assimp/revision.h
+++ third_party/assimp/include/assimp/revision.h
@@ -1,8 +1,8 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x116c96c4
-#define GitBranch "heads/master"
+#define GitVersion 0xa25a128c
+#define GitBranch "main"
 
 #define VER_MAJOR 6
 #define VER_MINOR 0
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 116c96c4)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit a25a128c)"
 #endif
 #define VER_COPYRIGHT_STR           "\xA9 2006-2023"
 
